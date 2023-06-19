3835a9ee377a1a0aa8a597306c0ab17bc368e058
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-06-19 17:10:30 -0300

    add code that works

--- index.js
+++ index.js
@@ -1,21 +1,62 @@
 const http = require('http');
 const fs = require('fs');
 
-const pages = {
-	'/': './pages/index.html'
-};
-
 http
 	.createServer((req, res) => {
-		const page = req.url;
-		fs.readFile(pages[page], (err, data) => {
+		if (req.url === '/') {
+			fs.readFile('./pages/index.html', (err, data) => {
+				if (err) throw err;
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write(data);
+				res.end();
+			});
+		} else if (req.url === '/about') {
+			fs.readFile('./pages/about.html', (err, data) => {
+				if (err) throw err;
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write(data);
+				res.end();
+			});
+		} else if (req.url === '/contact-me') {
+			fs.readFile('./pages/contact-me.html', (err, data) => {
+				if (err) throw err;
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write(data);
+				res.end();
+			});
+		} else {
+			fs.readFile('./pages/404.html', (err, data) => {
+				if (err) throw err;
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write(data);
+				res.end();
+			});
+		}
+	})
+	.listen(8080, () => {
+		console.log('Listen to port 8080 ....');
+	});
+
+/* const pages = {
+	'/': './pages/index.html',
+	'/about': './pages/about.html',
+	'/contact-me': './pages/contact-me.html',
+	'/404': './pages/404.html'
+};
+ */
+/* http
+	.createServer((req, res) => {
+		let page = req.url;
+		fs.readFile(path.join(__dirname, pages[page]), (err, data) => {
 			if (err) {
 				res.writeHead(404, { 'Content-Type': 'text/html' });
-				return res.end('404 Not Found');
+				res.end('404 Not Found');
+			} else {
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write(data);
+				res.end();
 			}
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.write(data);
-			return res.end();
 		});
 	})
 	.listen(8080);
+ */
