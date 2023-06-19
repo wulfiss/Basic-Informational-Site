df79674ec39f297b14503fc14df635c8b7abc655
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-06-19 16:20:56 -0300

    redirect to index to object

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const http = require('http');
 const fs = require('fs');
 
-http.createServer((req, res) =>{
-    if(req.url === '/'){
-        fs.readFile('./pages/index.html', (err, data) =>{
-            if(err){
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                return res.end('404 Not Found');
-            }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            return res.end();           
-        })
-    }
-}).listen(8080);
+const pages = {
+	'/': './pages/index.html'
+};
+
+http
+	.createServer((req, res) => {
+		const page = req.url;
+		fs.readFile(pages[page], (err, data) => {
+			if (err) {
+				res.writeHead(404, { 'Content-Type': 'text/html' });
+				return res.end('404 Not Found');
+			}
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write(data);
+			return res.end();
+		});
+	})
+	.listen(8080);
